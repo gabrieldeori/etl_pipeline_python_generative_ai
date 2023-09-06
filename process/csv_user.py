834f71caccd727fae17782bda4941bf834e07229
gabrieldeori <deori.pessoal@gmail.com>
2023-09-06 18:31:33 -0300

    refact: rename csv_user

--- process/csv_user.py
+++ process/csv_user.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def return_user_ids():
-    user_csv_data = pd.read_csv('data/user_info_2023.csv')
+def return_user_ids(path):
+    user_csv_data = pd.read_csv(paths)
     user_ids = user_csv_data['UserID'].tolist()
     return user_ids

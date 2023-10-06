51acd2040e5bb00a0cbd6b05129c81a50151a1ba
gabrieldeori <deori.pessoal@gmail.com>
2023-10-06 16:05:57 -0300

    feat: add flask to project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_file, render_template
+import api_generate
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def index():
+    return render_template('pages/initialPage/index.html')
+
+@app.route('/get_clients')
+def get_clients():
+    result = api_generate.startScript()
+    return f"<p>{result}</p>"
+
+if __name__ == '__main__':
+    app.run()
